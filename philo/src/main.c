c8d43e9a7256837d15d2df43d25f252e1eff91e3
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-03-23 09:27:02 +0900

    fix: print usage

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,11 +6,13 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 19:29:26 by iyamada           #+#    #+#             */
-/*   Updated: 2022/03/21 19:13:47 by iyamada          ###   ########.fr       */
+/*   Updated: 2022/03/23 08:37:53 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	print_usage(void);
 
 // __attribute__((destructor))
 // static void destructor() {
@@ -27,11 +29,13 @@ int	main(int argc, char *argv[])
 	t_error	err;
 
 	err = NO_ERR;
-	if (parse(argc, argv, &arg, &err) != 0
-		|| new_philo(&philo, &arg, &err) != 0)
+	if (parse(argc, argv, &arg, &err) != 0)
 	{
+		print_usage();
 		return (print_error(err));
 	}
+	if (new_philo(&philo, &arg, &err) != 0)
+		return (print_error(err));
 	if (init_philo(philo, &arg, &err) != 0
 		|| run_philo_thread(&philo, &err) != 0
 		|| run_monitor_thread(&philo, &err) != 0
