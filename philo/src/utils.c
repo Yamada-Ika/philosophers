d0ef6c0ab459cd12be79f50379c0c9d1391b12e3
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-04-03 03:04:50 +0900

    WIP

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 19:30:03 by iyamada           #+#    #+#             */
-/*   Updated: 2022/04/01 01:59:39 by iyamada          ###   ########.fr       */
+/*   Updated: 2022/04/03 02:58:30 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,23 +66,23 @@ static bool	is_someone_dead(t_philo *p)
 int	my_msleep(t_philo *philo, long long msec)
 {
 	long long	start;
-	size_t		loop_cnt;
+	// size_t		loop_cnt;
 
-	loop_cnt = 0;
+	// loop_cnt = 0;
 	start = get_timestamp();
 	while (true)
 	{
 		if (get_timestamp() - start >= msec)
 			return (SUCCESS);
-		if (loop_cnt % 10 == 0)
-		{
+		// if (loop_cnt % 100 == 0)
+		// {
 			if (is_someone_dead(philo))
 				return (FAIL);
-			loop_cnt++;
-			continue ;
-		}
-		usleep(500);
-		loop_cnt++;
+		// 	loop_cnt++;
+		// 	continue ;
+		// }
+		usleep(100);
+		// loop_cnt++;
 	}
 }
 
@@ -98,9 +98,11 @@ int	get_index(t_philo *philo, size_t index)
 
 int	print_action(t_philo *p, char *action)
 {
-	if (is_someone_dead(p)
-		|| mutex_lock(&p->mtxes[LOG], &p->mtxes[ERR], p->err) == FAIL)
+	if (mutex_lock(&p->mtxes[LOG], &p->mtxes[ERR], p->err) == FAIL)
+		return (FAIL);
+	if (is_someone_dead(p))
 	{
+		mutex_unlock(&p->mtxes[LOG], &p->mtxes[ERR], p->err);
 		return (FAIL);
 	}
 	printf("%lld %zu %s\n", get_timestamp(), p->index, action);
